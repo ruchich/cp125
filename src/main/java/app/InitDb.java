1e8b4b62dcbd44b79919062f409b6cf7beef5c25
ruchi chaudhary <ruchi.chaudhary@expeditors.com>
2017-03-20 16:58:21 -0700

    gfg

--- src/main/java/app/InitDb.java
+++ src/main/java/app/InitDb.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import com.scg.domain.ClientAccount;
 import com.scg.domain.Consultant;
+import com.scg.domain.ConsultantTime;
 import com.scg.domain.TimeCard;
 import com.scg.util.ListFactory;
 
@@ -52,16 +53,51 @@ public final class InitDb {
         		ps.executeUpdate();
                 
         	}
-        	ps = conn.prepareStatement("INSERT INTO consultants (last_name, first_name, middle_name) VALUES (?, ?,?)");
+        	/* Insert consultant */
+			ps = conn.prepareStatement("INSERT INTO consultants (last_name, first_name, middle_name) VALUES (?, ?,?)");
         	for(Consultant c:consultants){
         		ps.setString(1,c.getName().getLastName());
         		ps.setString(2,c.getName().getFirstName());
         		ps.setString(3,c.getName().getMiddleName());
         		ps.executeUpdate();
+
         		
         	}
-        	/* Select consultant id */
-        	rs = stmnt.executeQuery( "SELECT id FROM consultants  WHERE last_name = 'Architect'  AND first_name = 'Ann'  AND middle_name = 'S.'");
+        	/* Insert time cards */
+        	for(TimeCard t: timeCards) {
+				String lastName = t.getConsultant().getName().getLastName();
+				String firstName = t.getConsultant().getName().getFirstName();
+				String middleName = t.getConsultant().getName().getMiddleName();
+
+			/* Select consultant id */
+
+				rs = stmnt.executeQuery("SELECT id FROM consultants  WHERE last_name = lastName  AND first_name = firstName  AND middle_name = middleName");
+				int consultant_id = rs.getInt("consultant_id");
+				String startDate = t.getWeekStartingDay().toString();
+
+			/* Insert time card */
+
+				//INSERT INTO timecards (consultant_id, start_date)        VALUES (1, '2005/03/01');
+				ps = conn.prepareStatement("INSERT INTO timecards (consultant_id, start_date) VALUES (?, ?) ", Statement.RETURN_GENERATED_KEYS);
+				ps.setInt(1, consultant_id);
+				ps.setString(2, startDate);
+				ps.executeUpdate();
+				rs = ps.getGeneratedKeys();
+				rs.next();  // assume we successfully inserted
+				int timeCardID = rs.getInt(1);
+				/* Insert non-billable hours */
+				ps = conn.prepareStatement("INSERT INTO non_billable_hours (account_name, timecard_id, date, hours) VALUES (?, ?, ?, ?)");
+				for (final ConsultantTime currentTime : t.getConsultingHours()) {
+					if (!currentTime.isBillable()) {
+						ps.setString(1, currentTime.getAccount().getName());
+						ps.setInt(2, timeCardID);
+						ps.setString(3, currentTime.getDate().toString());
+						ps.setInt(4, currentTime.getHours());
+						ps.executeUpdate();
+					}
+				}
+			}
+
         	}  
         catch( SQLException ex )   {
         	System.out.println("Connection Failed");
